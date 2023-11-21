50cdf07cc779b87c5f4f4721994099f818b54495
PATO <c0b220992c@edu.teu.ac.jp>
2023-11-21 16:18:39 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -11,20 +11,22 @@ def main():
     bird_rct = bird_img.get_rect()
     bird_rct.center = 300, 200
     bird_img = pg.transform.flip(bird_img, True, False)
-    bird_imgs = [bird_img, pg.transform.rotozoom(bird_img, 10, 1.0)]
+    bird_imgs = [bird_img, pg.transform.rotozoom(bird_img, 1, 1.0),pg.transform.rotozoom(bird_img, 2, 1.0),pg.transform.rotozoom(bird_img, 3, 1.0),pg.transform.rotozoom(bird_img, 4, 1.0),pg.transform.rotozoom(bird_img, 5, 1.0),pg.transform.rotozoom(bird_img, 6, 1.0),pg.transform.rotozoom(bird_img, 7, 1.0),pg.transform.rotozoom(bird_img, 8, 1.0),pg.transform.rotozoom(bird_img, 9, 1.0), pg.transform.rotozoom(bird_img, 10, 1.0)]
+    pg.transform.rotozoom(bird_img, 1, 1.0),pg.transform.rotozoom(bird_img, 2, 1.0),pg.transform.rotozoom(bird_img, 3, 1.0),pg.transform.rotozoom(bird_img, 4, 1.0),pg.transform.rotozoom(bird_img, 5, 1.0),pg.transform.rotozoom(bird_img, 6, 1.0),pg.transform.rotozoom(bird_img, 7, 1.0),pg.transform.rotozoom(bird_img, 8, 1.0),pg.transform.rotozoom(bird_img, 9, 1.0)
+
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         x = tmr % 1600
-        print(x)
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
-        screen.blit(bird_imgs[tmr%2], bird_rct)
+        screen.blit(pg.transform.flip(bg_img, True, False), [1600-x, 0])
+        screen.blit(bird_imgs[tmr%10], bird_rct)
+        print(tmr%10)
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
