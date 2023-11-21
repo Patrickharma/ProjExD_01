cbdc6decf74d63347ab0df03e24a07b742f42755
PATO <c0b220992c@edu.teu.ac.jp>
2023-11-21 15:59:27 +0900

    演習３

--- pygame_image.py
+++ pygame_image.py
@@ -11,17 +11,20 @@ def main():
     bird_rct = bird_img.get_rect()
     bird_rct.center = 300, 200
     bird_img = pg.transform.flip(bird_img, True, False)
-    bird_img = pg.transform.rotozoom(bird_img, 10, 1.0)
+    bird_imgs = [bird_img, pg.transform.rotozoom(bird_img, 10, 1.0)]
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        screen.blit(bird_img, bird_rct)
+        x = tmr % 1600
+        print(x)
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(bird_imgs[tmr%2], bird_rct)
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
